df0aa3c444ea5900575e21a71a775cc34f8fa1c1
bendell02 <1203029076@qq.com>
2019-03-19 23:29:10 +0800

    solve the problem ...
    'populating-next-right-pointers-in-each-node-ii'

---
+++ 02_leetcode/35_populating-next-right-pointers-in-each-node-ii.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for binary tree with next pointer.
+ * struct TreeLinkNode {
+ *  int val;
+ *  TreeLinkNode *left, *right, *next;
+ *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void connect(TreeLinkNode *root) {
+        // boundary
+        if (NULL == root) {
+            return ;
+        }
+
+        queue<TreeLinkNode*> q;
+        q.push(NULL);
+        TreeLinkNode* current_node = NULL;
+        TreeLinkNode* previous_node = root;
+        while(!q.empty()) {
+            current_node = q.front(); q.pop();
+            previous_node->next = current_node;
+
+            if (NULL != previous_node->left) {
+                q.push(previous_node->left);
+            }
+            if (NULL != previous_node->right) {
+                q.push(previous_node->right);
+            }
+
+            if (NULL == current_node) {
+                q.push(NULL);
+                while((!q.empty()) && (NULL==q.front())) {
+                    q.pop();
+                }
+                if (!q.empty()) {
+                    previous_node = q.front(); q.pop();
+                }
+            }
+            else {
+                previous_node = current_node;
+            }
+        }
+    }
+};
