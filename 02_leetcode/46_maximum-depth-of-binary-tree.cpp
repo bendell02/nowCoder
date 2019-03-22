5bde76509d5633b9ddf85caa962dfd747de05e9f
bendell02 <1203029076@qq.com>
2019-03-22 17:54:54 +0800

    solve the "maximum-depth-of-binary-tree"

---
+++ 02_leetcode/46_maximum-depth-of-binary-tree.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode *root) {
+        if (NULL == root) {
+            return 0;
+        }
+ 
+        int left_depth = maxDepth(root->left);
+        int right_depth = maxDepth(root->right);
+ 
+ 
+        return (1 + std::max(left_depth, right_depth));
+    }
+};
