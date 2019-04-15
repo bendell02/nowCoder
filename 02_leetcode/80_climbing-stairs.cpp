86cb114d91abfc3a2f7767b093faf06fc67fd167
bendell02 <1203029076@qq.com>
2019-04-15 19:25:55 +0800

    solve the 'climbing-stairs'

---
+++ 02_leetcode/80_climbing-stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n <= 1) { return 1; }
+ 
+        int first = 1, second = 1;
+        int result = 0;
+        for (int i = 2; i <= n; i++) {
+            result = first + second;
+            first = second;
+            second = result;
+        }
+ 
+        return result;
+    }
+};
