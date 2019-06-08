f67147f150e8defe4941dd9c87b3383fb28064da
bendell02 <1203029076@qq.com>
2019-06-09 00:54:51 +0800

    solve the 'remove-element'

---
+++ 02_leetcode/124_remove_element.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int removeElement(int A[], int n, int elem) {
+        for (int i = 0; i < n; ++i) {
+            if (A[i] == elem) {
+                swap(A[i--], A[--n]);
+            }
+        }
+
+        return n;
+    }
+};
