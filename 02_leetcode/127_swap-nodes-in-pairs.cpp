b7f9cba23f750564f2add3d5b2f8ae848ee94062
bendell02 <1203029076@qq.com>
2019-06-11 20:46:09 +0800

    solve the 'swap-nodes-in-pairs'

---
+++ 02_leetcode/127_swap-nodes-in-pairs.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        return reverseKGroup(head, 2);
+    }
+     
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode* temp_head = head;
+        int index = 0;
+         
+        ListNode *first = new ListNode(-1); first->next = head;
+        ListNode *second = head;
+        while (NULL != second) {
+            index++;
+ 
+            if (index == k) {
+                temp_head = second;
+            }
+ 
+            if (0 == (index%k)) {
+                ListNode *temp = second->next; 
+ 
+                reverseList(first->next, second);
+                first->next->next = temp;
+                temp = first->next;
+                first->next = second;
+                second = temp;
+                first = temp;
+            }
+ 
+            second = second->next;
+        }
+ 
+        return temp_head;
+    }
+ 
+    ListNode* reverseList(ListNode *first, ListNode *second) {
+        ListNode* temp_head = NULL;
+        ListNode *post = first;
+        ListNode *target = second->next;
+ 
+        // change every node
+        while (target != post) {
+            first = post;
+            post = post->next;
+            first->next = temp_head;
+            temp_head = first;
+        }
+ 
+        return temp_head;
+    }
+};
